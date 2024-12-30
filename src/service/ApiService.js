65e81bb22ca3087c12b928a4bfdede45e4bd02e4
Sashsathish <123631053+Sashsathish@users.noreply.github.com>
2024-12-30 20:54:26 +0530

    Update ApiService.js

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class ApiService {
 
-    static BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+    static BASE_URL = https://velvet-horizon-latest.onrender.com
 
     static getHeader() {
         const token = localStorage.getItem("token");
